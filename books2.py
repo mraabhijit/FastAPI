8b21fafa8eb7f0f886dd2d86722ec54bf54a0b4b
abhijitchak103 <abhijityachak@gmail.com>
2024-06-17 16:14:01 +0530

    add custom exception for handling negative book counts in read_all_books

--- books2.py
+++ books2.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel, Field
 from uuid import UUID
 from typing import Union, Optional
+from starlette.responses import JSONResponse
 
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return: int):
+        self.books_to_return = books_to_return
 
 app = FastAPI()
 
@@ -33,8 +38,22 @@ class Book(BaseModel):
 BOOKS = []
 
 
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request,
+                                            exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Hey, why do you want {exception.books_to_return} "
+                            f"books? You need to read more!"}
+    )
+
+
 @app.get('/')
 async def read_all_books(books_to_return: Optional[int] = None):
+
+    if books_to_return and books_to_return < 0:
+        raise NegativeNumberException(books_to_return=books_to_return)
+
     if len(BOOKS) < 1:
         create_books_no_api()
     # Add functionality to return a fixed number of books
