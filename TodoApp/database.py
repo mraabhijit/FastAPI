a329b3b0ec4b60c6dcedf867bfa49f594aa2dca2
abhijitchak103 <abhijityachak@gmail.com>
2024-06-21 14:56:29 +0530

    add postgres db usage to database.py

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,12 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_SQLITE3_DATABASE_URL = "sqlite:///./todos.db"
+# Pattern = "db://username:password@hostadress/ApplicationDatabaseName"
+SQLALCHEMY_POSTGRES_DATABASE_URL = "postgresql://postgres:postgres@localhost/TodoAppDatabase"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False} # to allow multiple threads to use the same conn
-)
+
+db = 'postgres'
+
+if db == 'Sqlite3':
+    engine = create_engine(
+        SQLALCHEMY_SQLITE3_DATABASE_URL,
+        connect_args={'check_same_thread': False} # to allow multiple threads to use the same conn
+    )
+elif db == 'postgres':
+    engine = create_engine(
+        SQLALCHEMY_POSTGRES_DATABASE_URL
+    )
 
 SessionLocal = sessionmaker(
     autocommit=False, # autocommit controls transaction behavior
