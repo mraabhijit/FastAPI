3f2fe33f75b12fb5432c459d7b9c30f93556a839
abhijitchak103 <abhijityachak@gmail.com>
2024-06-25 15:16:43 +0530

    add edit todo post api, add priority to todos list home page

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -88,6 +88,29 @@ async def edit_todo(request: Request,
     )
 
 
+@router.post('/edit/{todo_id}', response_class=HTMLResponse)
+async def edit_todo_commit(request:Request, 
+                           todo_id: int,
+                           title:str = Form(), 
+                           description: str = Form(),
+                           priority: int = Form(),
+                           db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos) \
+                    .filter(models.Todos.id == todo_id) \
+                    .first()
+    
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(
+        url='/todos',
+        status_code=status.HTTP_302_FOUND
+    )
+
 
 
 
